a2a97fdd8c2e868fa6061f7438658ff241f1b944
Cbumann2 <cbumann@angelo.edu>
2023-10-08 17:16:10 -0500

    added help menu

---
+++ help.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+	"fyne.io/fyne/v2"
+	//"fyne.io/fyne/v2/layout"
+	//"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	//"go.bug.st/serial"
+	//"log"
+)
+
+//this function returns the UI for the help menu
+func HelpMenu(config *SerialConfig) fyne.CanvasObject {
+	return widget.NewLabel("This is the help menu!")
+}
