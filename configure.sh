f2083083b0837244bf4447dc2bae46d69751d3fb
Robert Burnett <dr.slate2@gmail.com>
2023-10-16 17:31:02 -0500

    updated configure.sh

--- configure.sh
+++ configure.sh
@@ -1,17 +1,25 @@
 if [ ! -d "go" ]; then
+   echo "[NO GO COMPILER FOUND]"
+   echo "[DOWNLOADING GO COMPILER]"
    curl -LO "https://go.dev/dl/go1.21.2.windows-amd64.zip"
-   unzip "go1.21.2.windows-amd64.zip"
+   echo "[UNZIPPING GO COMPILER]"
+   unzip "go1.21.2.windows-amd64.zip" 1>/dev/null
+   echo "[REMOVING ZIP FILE]"
    rm "go1.21.2.windows-amd64.zip"
 fi
 
 GOBIN="./go/bin/go.exe"
 
 if [ ! -d "w64devkit" ]; then
+   echo "[NO GCC FOUND]"
+   echo "[DOWNLOADING GCC]"
    curl -LO "https://github.com/skeeto/w64devkit/releases/download/v1.20.0/w64devkit-1.20.0.zip"
-   unzip "w64devkit-1.20.0.zip"
+   echo "[UNZIPPING GCC]"
+   unzip "w64devkit-1.20.0.zip" 1>/dev/null
+   echo "[REMOVING ZIP FILE]"
    rm "w64devkit-1.20.0.zip"
 fi
 
-CC="./w64devkit/bin/x86_64-w64-mingw32-gcc.exe"
-
-CGO_ENABLED=1 GOOS=windows $GOBIN build
+echo "[ATTEMPING TO COMPILE]"
+COMPILER="/w64devkit/bin/x86_64-w64-mingw32-gcc.exe" 
+CC="$PWD$COMPILER" CGO_ENABLED=1 GOOS=windows $GOBIN build
---
+++ model.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"github.com/mitchellh/go-homedir"
+	"go.bug.st/serial"
+	"os"
+)
+
+// this struct stores the configuration details that will be passed to the upload/download backend
+type SerialConfig struct {
+	Port     string
+	Settings serial.Mode
+}
+
+// The model of MVC, stores internal state
+type Model struct {
+	Config           SerialConfig
+	UploadFilepath   string
+	DownloadFilepath string
+}
+
+// sets the upload filepath
+func (self *Model) SetUploadFilepath(path string) error {
+	filepath, err := homedir.Expand(path)
+	if err != nil {
+		return err
+	}
+
+	self.UploadFilepath = filepath
+	return nil
+}
+
+// reads the value of the upload filepath
+func (self *Model) ReadUploadFile() (string, error) {
+	bytes, err := os.ReadFile(self.UploadFilepath)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+// initalizes with proper defaults
+func DefaultModel() Model {
+	return Model{
+		Config: SerialConfig{
+			Port: "",
+			Settings: serial.Mode{
+				BaudRate: 4800,
+				Parity:   serial.EvenParity,
+				DataBits: 8,
+				StopBits: serial.OneStopBit,
+			},
+		},
+		UploadFilepath:   "",
+		DownloadFilepath: "",
+	}
+}
+
+func (m *Model) Upload() (int, error) {
+	port, err := serial.Open(m.Config.Port, &m.Config.Settings)
+	if err != nil {
+		return 0, err
+	}
+
+	//TODO close the port
+	str, err := m.ReadUploadFile()
+	if err != nil {
+		return 0, err
+	}
+	return port.Write([]byte(str))
+}
